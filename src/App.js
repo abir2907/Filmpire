74925435f229a8e50e2f0a87d4b1c4325fd5988d
Abir Chodha <abirchodha1@gmail.com>
2024-09-03 06:44:52 +0530

    Create searchMovies() to get movies from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?apikey=260ec8dd";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
   return <h1>App</h1>;
 };
 
