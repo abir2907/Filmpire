82aa69cf10eb306b38b080b60b536b44604c121e
Abir Chodha <abirchodha1@gmail.com>
2024-09-03 21:19:44 +0530

    Create a single movie card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=260ec8dd";
 
+const movie1 = {
+  Title: "Spiderman and Grandma",
+  Year: "2009",
+  imdbID: "tt1433184",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg",
+};
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -25,6 +34,26 @@ const App = () => {
           onChange={() => {}}
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>{movie1.Year}</div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
